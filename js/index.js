c09265e9adfc7b5deaad4442ab934e486572a6b6
liling <626447188@qq.com>
2023-06-30 19:54:36 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,151 @@
+// 解决 click 事件的300ms延迟问题
+FastClick.attach(document.body);
+
+(async function () {
+  const baseBox = document.querySelector('.header-box .base'),
+    innerBox = document.querySelector('.inner'),
+    wrapperBox = document.querySelector('.wrapper1'),
+    wrapper = document.querySelector('.cover .wrapper'),
+    footerBox = document.querySelector('.footer-box'),
+    currentBox = footerBox.querySelector('.current'),
+    durationBox = footerBox.querySelector('.duration'),
+    alreadyBox = footerBox.querySelector('.already'),
+    markImageBox = document.querySelector('.mark-image'),
+    loadingBox = document.querySelector('.loading-box'),
+    audioBox = document.querySelector('#audioBox');
+  let wrapperList = [], timer = null, matchNum = 0
+console.log(innerBox);
+  /* 音乐控制 */
+  const format = function format(time) {
+    let minutes = Math.floor(time / 60),
+      seconds = Math.round(time - minutes * 60)
+      minutes = minutes < 10 ? '0' + minutes : '' + minutes
+      seconds = seconds < 10 ? '0' + seconds : '' + seconds
+      return {
+        minutes,
+        seconds
+      }
+  }
+  const playend = function playend(){
+    clearInterval(timer)
+    timer = null
+    currentBox.innerHTML = '00:00'
+    alreadyBox.style.width = '0%'
+    wrapperBox.style.transform = 'translateY(0)'
+    wrapper.classList.remove('move')
+    wrapperList.forEach(item => item.className = '')
+    matchNum = 0
+    /* playerButton.className = 'player-button' */
+  }
+  const handle = function handle() {
+    let pH = wrapperList[0].offsetHeight
+    let { currentTime, duration } = audioBox
+    if (isNaN(currentTime) || isNaN(duration)) return
+    // 播放结束
+    if (currentTime >= duration) {
+      playend()
+      return
+    }
+    let { minutes: currentTimeMinutes, seconds: currentTimeSeconds } = format(currentTime), /*  加： 是重命名 */
+      { minutes: durationMinutes, seconds: durationSeconds } = format(duration),
+      ratio = Math.round(currentTime / duration * 100)
+    // 控制进度条
+    currentBox.innerHTML = `${currentTimeMinutes}:${currentTimeSeconds}`
+    durationBox.innerHTML = `${durationMinutes}:${durationSeconds}`
+    alreadyBox.style.width = `${ratio}%`
+      // 控制歌词：查找和当前播放时间匹配的歌词段落
+      let matchs = wrapperList.filter(item => {
+        let minutes = item.getAttribute('minutes'),
+          seconds = item.getAttribute('seconds')
+        return minutes === currentTimeMinutes && seconds === currentTimeSeconds
+      })
+      if (matchs.length > 0) {
+  
+        // 让匹配的段落有选中的样式，而其余的移除选中样式
+        wrapperList.forEach(item => item.className = '')
+        matchs.forEach(item => item.className = 'active')
+        // 控制移动
+        matchNum += matchs.length
+        if (matchNum > 3) {
+          let offset = (matchNum - 3) * pH
+          wrapperBox.style.transform = `translateY(${-offset}px)`;
+        }
+      }
+    };
+    innerBox.addEventListener('click', function () {
+      if (audioBox.paused) {
+        // 当前是暂停的：我们让其播放
+        audioBox.play()
+        handle()
+        if (!timer) timer = setInterval(handle, 1000)
+        wrapper.classList.add('move')
+        wrapper.style.animationPlayState = 'running'
+        innerBox.style.opacity = '0'
+        // playerButton.className='player-button move'
+        return
+      }
+      // 当前是播放的：我们让其暂停
+      audioBox.pause()
+      wrapper.style.animationPlayState = 'paused'
+      clearInterval(timer)
+      /* wrapper.classList.remove('move') */
+      innerBox.style.opacity = '1'
+      // playerButton.className='player-button'
+      timer = null
+    })
+
+  /* 绑定数据 */
+  const bindLyric = function bindLyric(lyric) {
+    // 解析歌词信息
+    let arr = [];
+    lyric.replace(/\[(\d+):(\d+).(?:\d+)\](.+)\n/g, (_, $1, $2, $3) => {
+      arr.push({
+        minutes: $1,
+        seconds: $2,
+        text: $3
+      });
+    })
+    // 歌词绑定
+    let str = ``;
+    arr.forEach(({ minutes, seconds, text }) => {
+      str += `
+      <p minutes="${minutes}" seconds="${seconds}">
+      ${text}</p>
+    `
+    })
+    wrapperBox.innerHTML = str
+
+    // 获取所有的p标签
+    wrapperList = Array.from(wrapperBox.querySelectorAll('p'))
+  }
+
+  const binding = function binding(data) {
+    let { title, author, duration, pic1, pic2, pic3, audio, lyric } = data;
+
+    //@2 杂七杂八的信息
+    durationBox.innerHTML = duration
+    markImageBox.style.backgroundImage = `url(${pic3})`;
+    audioBox.src = audio
+
+    //@3 绑定歌词信息
+    bindLyric(lyric)
+
+    //@4 关闭Loading效果
+    loadingBox.style.display = 'none'
+  }
+
+  /* 向服务器发送请求，从服务器获取相关的数据 */
+  try {
+    let { code, data } = await API.queryLyric()
+    if (+code === 0) {
+      // 请求成功：网络层和业务层都成功
+      binding(data)
+      return
+    }
+  } catch (_) {
+    // 请求失败
+    alert('网络繁忙，请刷新页面')
+    console.log(_);
+  }
+
+})()
